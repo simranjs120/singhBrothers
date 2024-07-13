d96ba181b1f4815ec2b4cb87c89639319c9927e7
simranjs120 <simranjs5041@gmail.com>
2024-07-14 00:50:06 +0530

    Component small spelling issue fix-4

--- app/View/Components/adminHeader.php
+++ app/View/Components/adminHeader.php
@@ -22,6 +22,6 @@ class AdminHeader extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.AdminHeader');
+        return view('components.Admin-header');
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,4 @@
-<x-AdminHeader :profile="$profile"/>
+<x-Admin-header :profile="$profile"/>
           <div class="row">
             <div class="col-sm-12">
               <div class="home-tab">

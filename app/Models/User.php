e6ce044398e40ec2cbe704a8074de88d6ed3c542
simranjs120 <simranjs5041@gmail.com>
2024-07-13 21:54:51 +0530

    Forgot password URL slash bug re-fix-2

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'forgot_password_key',
     ];
 
     /**
@@ -30,6 +31,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'forgot_password_key',
     ];
 
     /**
